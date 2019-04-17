60084fc18c2a13d7601dc746723b90f45ce0b186
novandev <donovan.adams@students.makeschool.com>
2019-04-17 11:38:14 -0700

    solved sock merchant

--- python/Interview_prep/sock_merchant.py
+++ python/Interview_prep/sock_merchant.py
@@ -4,12 +4,14 @@ This fucntion wlll count the numer of pairs of socks in an array that denotes co
 
 def sock_counter(sock_list:list) -> list:
     sock_dict = {}
+    matches = 0
     for sock in sock_list: # O(n) for going through the whoe list
         if sock not in sock_dict: #0(1) for lookup
             sock_dict[sock] = 0
         sock_dict[sock] += 1
-
-    return [sock for sock in sock_dict if sock_dict[sock] == 2 ] O(n)
+        if sock_dict[sock] % 2 == 0:
+            matches += 1
+    return matches
 
 if __name__ == "__main__":
     print(sock_counter([1,2,1,2,1,4,5,6,3]))
