c15ca29913640e4993b7d0580c6faa1e26f09a99
novandev <donovan.adams@students.makeschool.com>
2019-04-17 15:59:33 -0700

    started on counting clouds

--- python/Interview_prep/counting_clouds.py
+++ python/Interview_prep/counting_clouds.py
@@ -10,12 +10,12 @@ def counting_clouds(cloud_array: list) -> int:
     Params: cloud_array - An array of binary numbers
     Return: best_step_count - An integer of the best recorded steps a path might have
     """
-    total_step_count = [len([cloud for clould in cloud_array if cloud == 0])]
+    total_step_count = [len([cloud for cloud in cloud_array if cloud == 0])]
     skip = False
     for i in range(len(cloud_array)-2):
         if cloud_array[i] == 0 and cloud_array[i] == cloud_array[i +1]:
             skip = True
-            
+
 
 
 
