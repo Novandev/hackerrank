c16f08d854ee425a8fa4811ab0a5b30c8ea7b61c
novandev <donovan.adams@students.makeschool.com>
2019-04-18 13:28:15 -0700

    Solved with some internet help

--- python/Interview_prep/repeated_string.py
+++ python/Interview_prep/repeated_string.py
@@ -1,22 +1,20 @@
 
 
 def repeatedString(s, n):
-    number_of_a ={'a':0}
+    number_of_a =0
     sentinel = len(s) -1
     str_check = 0
     long_str = ''
     
-    for i in range(n):
-        if str_check > sentinel:
-            str_check = 0
-        if s[str_check] =='a':
-            number_of_a['a'] += 1
-        print(s[str_check])
-        long_str += s[str_check]
-        str_check +=1
-        # print(str_check)
-    print(long_str)
-    return number_of_a['a']
-
+    # for i in range(n):
+    #     if str_check > sentinel:
+    #         str_check = 0
+    #     if s[str_check] =='a':
+    #         number_of_a += 1
+    #     long_str += s[str_check]
+    #     str_check +=1
+    # return number_of_a
+    return sum(x == 'a' for x in s) * (n//len(s)) + sum(x == 'a' for x in s[:n%len(s)])
 if __name__ == "__main__":
-    print(repeatedString('aba', 10))
+    print(repeatedString('kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm'
+, 7367789))
