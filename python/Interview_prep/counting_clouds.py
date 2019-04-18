fa3753f616fc55d25c0ff1c4fbd21b19e8ddcdd9
novandev <donovan.adams@students.makeschool.com>
2019-04-17 21:08:39 -0700

    SOLVED

--- python/Interview_prep/counting_clouds.py
+++ python/Interview_prep/counting_clouds.py
@@ -10,18 +10,23 @@ def counting_clouds(cloud_array: list) -> int:
     Params: cloud_array - An array of binary numbers
     Return: best_step_count - An integer of the best recorded steps a path might have
     """
-    total_step_count = 0 # Total step count
-    counter = 0 # sentinel to keep a counter going to track positions ahead of it
-    arr_len = len(cloud_array) -1 # need a value to make sure we dont run over index in list
-    while counter < arr_len: # while the counter value is less than the array length
-        if counter + 2 < arr_len and cloud_array[counter + 2] == 0: # Check two position ahead and check if the next is a 0
-            counter += 2 # increase the counter
-            total_step_count += 1 # increase total count
-        else:
-            counter += 1
+    total_step_count = 0
+    skip_count = 0
+    for cloud in range(len(cloud_array) - 2):
+    
+        if cloud_array[cloud] == 1 or skip_count > 0:
+            skip_count -= 1
+            continue
+       
+        if cloud_array[cloud] == 0 and cloud_array[cloud + 1] == 0:
             total_step_count += 1
-    return total_step_count
+            skip_count += 1
+        elif cloud_array[cloud] == 0:
+            total_step_count += 1
+        else:
+            continue
 
+    return total_step_count
 
 if __name__ == "__main__":
     print(counting_clouds([0,1,0,0,0,1,0]))
