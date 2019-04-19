93f36b5264431543ac09f2019bf25e74526cadd2
novandev <donovan.adams@students.makeschool.com>
2019-04-19 15:42:46 -0700

    solved need to refractor

--- python/Interview_prep/array_2d_hour_glass.py
+++ python/Interview_prep/array_2d_hour_glass.py
@@ -23,11 +23,15 @@ def hourglass_max_sum(d_array: list) -> int:
 
         for pos in hour_glass:
             sum += d_array[pos[0]][pos[1]]
-        
+        print(sum)
+        print(max)
+        if sum > max:
+            max = sum
+        sum = 0
         for couple in hour_glass:
             couple[1] += 1
         
-    print(sum)
+    print(max)
         
 if __name__ == "__main__":
     test_list = [
