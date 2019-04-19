e5cdf4925e76327fe15e05d9836f6bd26fa5813b
novandev <donovan.adams@students.makeschool.com>
2019-04-19 15:39:50 -0700

    added more and almost finished

--- python/Interview_prep/array_2d_hour_glass.py
+++ python/Interview_prep/array_2d_hour_glass.py
@@ -3,33 +3,31 @@
 
 THis function should take a random 6x6 array and postion wise get a sum of all of the elements in the
 """
+import traceback
 
 def hourglass_max_sum(d_array: list) -> int:
     # print(d_array)
     hour_glass =[ [0,0], [0, 1], [0, 2], [1, 1], [2, 0], [2, 1], [2,2] ] 
     hour_glass_reset =[0, 1, 2, 1, 0, 1, 2]
-    horizontal= 0
-    vertical = 0
     sum =0
     stop= [5,5]
+    max =0
     while hour_glass[len(hour_glass) -1] != stop:
-        # print(hour_glass)
+        if hour_glass[len(hour_glass) - 1][1] >= 5:
 
-        if hour_glass[len(hour_glass) - 1][1] == 5:
-            for i in range(len(hour_glass) -1):
-                print(hour_glass[i])
-                hour_glass[i][1] = hour_glass_reset[i]
-            print(hour_glass)
             for couple in hour_glass:
                 couple[0] += 1
-            print(hour_glass)
+
+            for i in range(len(hour_glass)):
+                hour_glass[i][1] = hour_glass_reset[i]
 
         for pos in hour_glass:
-            # print(pos[1])
             sum += d_array[pos[0]][pos[1]]
+        
         for couple in hour_glass:
             couple[1] += 1
-        horizontal +=1 
+        
+    print(sum)
         
 if __name__ == "__main__":
     test_list = [
