60f9e1f6e6659ee0fbcfd0405dfacdbf075eca78
Novan Adams <novanadams35@gmail.com>
2025-04-18 13:53:37 -0700

    add/ finished 3 so far

--- Easy/03_python_arithmetic_operators.py
+++ Easy/03_python_arithmetic_operators.py
@@ -5,6 +5,17 @@ def arithmetic_operators(a, b):
         The first line contains the sum of the two numbers.
         The second line contains the difference of the two numbers (first - second).
         The third line contains the product of the two numbers.
+
+
+        Parameters
+        ----------
+        a : the first variable
+        b: the second variable
+
+        Returns
+        ----------
+        String
+            A representation of the three operators 
     """
     return f"{a + b}\n{a-b}\n{a*b}"
 
