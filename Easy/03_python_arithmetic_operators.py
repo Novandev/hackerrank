c7ef72eaccd382664eafdbc6c134cb69f6e9b86e
Novan Adams <novanadams35@gmail.com>
2025-04-18 13:51:20 -0700

    add/ arithmetic operators done

---
+++ Easy/03_python_arithmetic_operators.py
@@ -0,0 +1,13 @@
+
+
+def arithmetic_operators(a, b):
+    """
+        The first line contains the sum of the two numbers.
+        The second line contains the difference of the two numbers (first - second).
+        The third line contains the product of the two numbers.
+    """
+    return f"{a + b}\n{a-b}\n{a*b}"
+
+
+
+print(arithmetic_operators(5,7))
